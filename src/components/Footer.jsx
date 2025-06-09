1f7da4f6c5e3291bdcce2aebac8a98b475ec99b0
Rishabh Nema <rishinema2105@gmail.com>
2025-06-09 20:48:23 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Logo from './Logo'
 import { FaGithub } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+    const navigate = useNavigate();
   return (
         <footer className="w-full px-6 py-6 border-t-[1px] border-slate-800 ">
         <div className="flex flex-col sm:flex-row gap-5 sm:gap-0 sm:justify-between px-4">
@@ -23,9 +25,11 @@ const Footer = () => {
 
           <div className="w-full mt-5 sm:mt-0 sm:w-1/2 text-center sm:text-end">
             <h1 className="text-3xl">One <span className="text-sky-600 font-anton">Pomodoro</span> at a time!</h1>
+            <a href="https://github.com/rishabhnema21/PomoManage" target="_blank" rel="noopener noreferrer">
             <button className="px-7 sm:px-9 sm:text-2xl hover:bg-sky-950 transition-all ease-in-out duration-200 rounded-2xl py-2 text-lg sm:py-3 bg-sky-900 mt-4">
               <FaGithub />
             </button>
+            </a>
             <div className="text-sm mt-3">
               <p>&copy; 2025 PomoManage. All rights reserved.</p>
               <p>Made with &#10084; by Rishabh Nema </p>
